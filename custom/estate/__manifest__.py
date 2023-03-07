2cda57e35de227a448fb9bfc40cf148a53134358
cloud <cloudintheskyoutlook.es>
2023-03-07 08:27:12 -0500

    Model fields added

--- custom/estate/__manifest__.py
+++ custom/estate/__manifest__.py
@@ -7,17 +7,19 @@
     'description': """
     Description text
     """,
+
+    
+     # add the property 'application' so my module can be considered a full app in odoo
     'application': True,
+    'installable': True,
 
     # data files always loaded at installation
     'data': [
-        'views/mymodule_view.xml',
+        
     ],
     # data files containing optionally loaded demonstration data
     'demo': [
-        'demo/demo_data.xml',
+        
     ],
-
-    # add the property 'application' so my module can be considered a full app in odoo
 
 }
